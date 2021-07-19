15523c35dc3ed9602781879931b5b6fbb50d5aef
bitomic <35199700+bitomic@users.noreply.github.com>
2021-07-19 11:52:33 -0600

    fix: named parameter's name is a Text, not string

--- src/models/NamedParameter.ts
+++ src/models/NamedParameter.ts
@@ -1,19 +1,20 @@
 import { NodeList } from '../utils'
 import { TemplateParameter } from './_TemplateParameter'
+import { Text } from './Text'
 import { Token } from './_Token'
 
 export class NamedParameter extends TemplateParameter {
-	#name: string
+	#name: Text
 	#value: NodeList
 	
-	constructor( { name, value }: { name: string, value: Token[] } ) {
+	constructor( { name, value }: { name: Text, value: Token[] } ) {
 		super()
 		this.#name = name
 		this.#value = new NodeList( value )
 	}
 
-	get name(): string { return this.#name }
-	set name( name: string ) { this.#name = name }
+	get name(): string { return this.#name.value }
+	set name( name: string ) { this.#name.value = name }
 
 	get value(): NodeList { return this.#value }
 	set value( nodes: NodeList ) { this.#value = nodes }
