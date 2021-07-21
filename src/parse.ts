d6da1b7155dddcd94f15d79f05bd259ffc66d476
bitomic <35199700+bitomic@users.noreply.github.com>
2021-07-20 21:56:13 -0600

    fix: load grammar using relative path

--- src/parse.ts
+++ src/parse.ts
@@ -1,9 +1,10 @@
 import { NodeList } from './utils'
 import fs from 'fs'
+import path from 'path'
 import peg from 'pegjs'
 import * as Models from './models'
 
-const grammar = fs.readFileSync( './grammar.pegjs' ).toString()
+const grammar = fs.readFileSync( path.resolve( __dirname, '../grammar.pegjs' ) ).toString()
 const parser = peg.generate( grammar )
 
 export const parse = ( text: string ): NodeList<Models.Token> => new NodeList( parser.parse( text, Models ) )
