3c51ffab637ed199158cef1c37a266aa999a359a
bitomic <35199700+bitomic@users.noreply.github.com>
2021-10-05 11:43:03 -0600

    feat: find template from node list

--- src/structures/utils/NodeList.ts
+++ src/structures/utils/NodeList.ts
@@ -8,6 +8,19 @@ export class NodeList<T extends Token = Token> {
 		this.nodes = nodes
 	}
 
+	public findTemplate( name: string ): NodeList<Template> {
+		const underscored = name.trim().replace( / /g, '_' )
+		const nodes: Template[] = []
+
+		for ( const node of this.nodes ) {
+			if ( node instanceof Template && ( node.name === name || node.name === underscored ) ) {
+				nodes.push( node )
+			}
+		}
+
+		return new NodeList( ...nodes )
+	}
+
 	public get templates(): NodeList<Template> {
 		const nodes: Template[] = []
 		for ( const node of this.nodes ) {
