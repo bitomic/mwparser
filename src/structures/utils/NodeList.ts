397f9ca34547a8a485396a438098ce85d4dfc3d8
bitomic <35199700+bitomic@users.noreply.github.com>
2021-10-04 21:58:55 -0600

    feat: get templates from node list

--- src/structures/utils/NodeList.ts
+++ src/structures/utils/NodeList.ts
@@ -1,3 +1,4 @@
+import { Template } from '../Template'
 import { Token } from '../Token'
 
 export class NodeList<T extends Token = Token> {
@@ -5,6 +6,14 @@ export class NodeList<T extends Token = Token> {
 
 	constructor( ...nodes: T[] ) {
 		this.nodes = nodes
+	}
+
+	public get templates(): NodeList<Template> {
+		const nodes: Template[] = []
+		for ( const node of this.nodes ) {
+			if ( node instanceof Template ) nodes.push( node )
+		}
+		return new NodeList( ...nodes )
 	}
 
 	public toString(): string {
