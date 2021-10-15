727b9a2d6b35fbace0f60e990de821ca52cfef5c
bitomic <35199700+bitomic@users.noreply.github.com>
2021-10-15 16:13:16 -0600

    fix: better trimming on text's value reassign, and special condition for trailing line break

--- src/structures/Text.ts
+++ src/structures/Text.ts
@@ -15,12 +15,18 @@ export class Text extends Token {
 	}
 
 	public set value( value: string ) {
+		if ( this.rawValue.trim().length === 0 && this.rawValue.endsWith( '\n' ) ) {
+			const leftTrail = this.rawValue.slice( 0, this.rawValue.length - 1 )
+			this.rawValue = `${ leftTrail }${ value }\n`
+			return
+		}
+
 		const leftTrail = this.rawValue.substr(
 			0,
 			this.rawValue.length - this.rawValue.trimLeft().length
 		)
 		const rightTrail = this.rawValue.substr(
-			this.rawValue.trimRight().length,
+			this.rawValue.length - this.rawValue.trimRight().length,
 			this.rawValue.length
 		)
 		this.rawValue = `${ leftTrail }${ value }${ rightTrail }`
